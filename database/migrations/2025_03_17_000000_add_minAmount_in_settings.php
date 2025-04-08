9da722410510a121beb760b6ceba857f26d91f13
Maria_Jose_Olivero <majolivero28@gmail.com>
2025-04-08 16:21:35 -0500

    Direcciones Duplicadas

--- database/migrations/2025_03_17_000000_add_minAmount_in_settings.php
+++ database/migrations/2025_03_17_000000_add_minAmount_in_settings.php
@@ -7,17 +7,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up() {
-        DB::table('settings')->insert([
-            'id' => 6,  
-            'name' => 'Monto Minimo',
-            'key' => 'min_amount',
-            'value' => '100000',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        $exists = DB::table('settings')->where('key', 'min_amount')->exists();
+
+        if (!$exists) {
+            DB::table('settings')->insert([
+                'id' => 6,  
+                'name' => 'Monto Minimo',
+                'key' => 'min_amount',
+                'value' => '100000',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 
     public function down() {
-        DB::table('settings')->where('id', 6)->delete();
+        DB::table('settings')->where('key', 'min_amount')->delete();
     }
 };
