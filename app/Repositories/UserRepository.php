20a8a8e784c94c544787aaac5f0ecd199be7ba96
David Correa <david.correav@gmail.com>
2025-04-28 20:53:19 -0500

    Add zona

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -54,7 +54,7 @@ class UserRepository
         }
 
         $token = $token->value;
-        //$zone = $zone ?? '';
+        $zone = $zone ?? '';
 
         //901703447
         $body = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:dat="http://schemas.microsoft.com/dynamics/2013/01/datacontracts" xmlns:tem="http://tempuri.org" xmlns:dyn="http://schemas.datacontract.org/2004/07/Dynamics.AX.Application">
@@ -73,7 +73,7 @@ class UserRepository
                         <!--Optional:-->
                         <dyn:IdentificationNum>' . $document . '</dyn:IdentificationNum>
                         <!--Optional:-->
-                        <dyn:ruteroId></dyn:ruteroId>
+                        <dyn:ruteroId>' . $zone . '</dyn:ruteroId>
                         <!--Optional:-->
                         <dyn:zona>' . $zone . '</dyn:zona>
                     </tem:_getRuteros>
