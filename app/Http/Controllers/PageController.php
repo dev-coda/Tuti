f290b4e68b14ba93c5196faa2ff90c5966ccd28e
Maria_Jose_Olivero <majolivero28@gmail.com>
2025-05-05 01:18:08 -0500

    correccion de los filtros y ordenamiento

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -26,86 +26,76 @@ class PageController extends Controller
     }
 
     public function search(Request $request, $order = '1', $category_id = '0', $brand_id = '0')
-    {
+{
+    $brands = Brand::whereActive(1)->orderBy('name')->get();
+    $categories = Category::with('parent')->get();
 
-        $brands = Brand::whereActive(1)->orderBy('name')->get();
-        $categories = Category::with('parent')->get();
-        $productBrandIds = null;
+    $transliterator = Transliterator::createFromRules(':: NFD; :: [:Mn:] Remove; :: NFC;');
+    $q = $request->input('q');
+    $q = $transliterator->transliterate($q);
 
-        $transliterator = Transliterator::createFromRules(':: NFD; :: [:Mn:] Remove; :: NFC;');
-        $q = $request->input('q');
-        $q = $transliterator->transliterate($q);
+    $params = compact('q', 'order', 'category_id', 'brand_id');
 
-        $params = compact('q', 'order', 'category_id', 'brand_id');
+    $productsQuery = Product::active()->where(function ($query) use ($q) {
+        $query->whereRaw("unaccent(name) ILIKE ?", ['%' . $q . '%'])
+            ->orWhereRaw("unaccent(description) ILIKE ?", ['%' . $q . '%'])
+            ->orWhereRaw("unaccent(lower(short_description)) ILIKE ?", ['%' . $q . '%'])
+            ->orWhereRaw("unaccent(lower(sku)) ILIKE ?", ['%' . $q . '%']);
+    });
 
-        $products = Product::active()->where(function ($query) use ($q) {
+    if ($category_id) {
+        $productsQuery->whereHas('categories', function ($query) use ($category_id) {
+            $query->where('category_id', $category_id);
+        });
+    }
+
+    if ($brand_id) {
+        $productsQuery->where('brand_id', $brand_id);
+    }
+
+    $productBrandIds = Product::active()
+        ->where(function ($query) use ($q) {
             $query->whereRaw("unaccent(name) ILIKE ?", ['%' . $q . '%'])
                 ->orWhereRaw("unaccent(description) ILIKE ?", ['%' . $q . '%'])
                 ->orWhereRaw("unaccent(lower(short_description)) ILIKE ?", ['%' . $q . '%'])
                 ->orWhereRaw("unaccent(lower(sku)) ILIKE ?", ['%' . $q . '%']);
-        });
+        })
+        ->pluck('brand_id')->toArray();
 
-        $productBrandIds = $products->pluck('brand_id')->toArray();
+    $brands = $brands->filter(fn($brand) => in_array($brand->id, $productBrandIds));
 
-        $brands = $brands->filter(function ($brand) use ($productBrandIds) {
-            if (in_array($brand->id, $productBrandIds)) {
-                return $brand;
-            } else {
-                return false;
-            };
-        });
-
-        $categoriesArray = [];
-
-        $productCategories = $products->each(function ($item) use (&$categoriesArray) {
-            $values = join(',', $item->categories()->pluck('id')->toArray());
-            array_push($categoriesArray, $values);
-        });
-
-
-        $categories = $categories->filter(function ($category) use ($categoriesArray) {
-            if (in_array($category->id, $categoriesArray)) {
-                return $category;
-            } else {
-                return false;
-            };
-        });
-
-
-        if ($category_id) {
-            $products = $products->whereHas('categories', function ($query) use ($category_id) {
-                $query->where('category_id', $category_id);
-            });
-        }
-
-        if ($brand_id) {
-            $products = $products->where('brand_id', $brand_id);
-        }
-
-        switch ($order) {
-            case 1:
-                $products = $products->orderBy('created_at', 'desc')->paginate();
-                break;
-            case 2:
-                $products = $products->orderBy('price', 'asc')->paginate();
-                break;
-            case 3:
-                $products = $products->orderBy('price', 'desc')->paginate();
-                break;
-            case 4:
-                $products = $products->orderBy('name', 'asc')->paginate();
-                break;
-            case 5:
-                $products = $products->orderBy('name', 'desc')->paginate();
-                break;
-            default:
-                $products = $products->paginate();
-                break;
-        }
-
-        $context = compact('products', 'brands', 'categories', 'params');
-        return view('pages.search', $context);
+    switch ($order) {
+        case 1:
+            $products = $productsQuery->orderBy('created_at', 'desc')->paginate();
+            break;
+        case 2:
+            $products = $productsQuery->orderBy('price', 'asc')->paginate();
+            break;
+        case 3:
+            $products = $productsQuery->orderBy('price', 'desc')->paginate();
+            break;
+        case 4:
+            $products = $productsQuery->orderBy('name', 'asc')->paginate();
+            break;
+        case 5:
+            $products = $productsQuery->orderBy('name', 'desc')->paginate();
+            break;
+        default:
+            $products = $productsQuery->paginate();
+            break;
     }
+
+    $categoriesArray = [];
+    foreach ($products as $item) {
+        $values = $item->categories()->pluck('id')->toArray();
+        $categoriesArray = array_merge($categoriesArray, $values);
+    }
+
+    $categories = $categories->filter(fn($category) => in_array($category->id, $categoriesArray));
+
+    return view('pages.search', compact('products', 'brands', 'categories', 'params'));
+}
+
 
     public function product($slug)
     {
@@ -136,94 +126,77 @@ class PageController extends Controller
     }
 
     public function category($slug, $slug2 = '0', $order = '1', $category_id = '0', $brand_id = '0')
-    {
-        $brands = Brand::whereActive(1)->orderBy('name')->get();
-        $banners = Banner::whereTypeId(1)->orderBy('id')->get();
-        $categories = Category::with('parent')->get();
-        $params = compact('slug', 'slug2', 'order', 'category_id', 'brand_id', 'banners');
+{
+    $brands = Brand::whereActive(1)->orderBy('name')->get();
+    $banners = Banner::whereTypeId(1)->orderBy('id')->get();
+    $categories = Category::with('parent')->get();
+    $params = compact('slug', 'slug2', 'order', 'category_id', 'brand_id', 'banners');
 
-        if ($slug2) {
-            $category = Category::with('parent')->where('slug', $slug2)->firstOrFail();
+    $category = $slug2
+        ? Category::with('parent')->where('slug', $slug2)->firstOrFail()
+        : Category::with('children')->where('slug', $slug)->firstOrFail();
 
-        } else {
 
-            $category = Category::where('slug', $slug)->firstOrFail();
-            $products = $category->products();
-
-            //seleccion el id de las categorias padres 
-            $ids = $category->children->pluck('id')->toArray();
-
-            $ids = array_merge($ids, [$category->id]);
-
-            $products = Product::active()->whereHas('categories', function ($query) use ($ids) {
-                $query->whereIn('category_id', $ids);
-            });
-        }
-
-        $productBrandIds = $products->pluck('brand_id')->toArray();
-
-        $brands = $brands->filter(function ($brand) use ($productBrandIds) {
-            if (in_array($brand->id, $productBrandIds)) {
-                return $brand;
-            } else {
-                return false;
-            };
+    if ($slug2) {
+        $productsQuery = Product::active()->whereHas('categories', function ($query) use ($category) {
+            $query->where('category_id', $category->id);
         });
+    } else {
+        $ids = $category->children->pluck('id')->toArray();
+        $ids[] = $category->id;
 
-        $categoriesArray = [];
-
-        $productCategories = $products->each(function ($item) use (&$categoriesArray) {
-            $values = join(',', $item->categories()->pluck('id')->toArray());
-            array_push($categoriesArray, $values);
+        $productsQuery = Product::active()->whereHas('categories', function ($query) use ($ids) {
+            $query->whereIn('category_id', $ids);
         });
-
-
-        $categories = $categories->filter(function ($category) use ($categoriesArray) {
-            if (in_array($category->id, $categoriesArray)) {
-                return $category;
-            } else {
-                return false;
-            };
-        });
-
-        if ($category_id) {
-            $products = $products->whereHas('categories', function ($query) use ($category_id) {
-                $query->where('category_id', $category_id);
-            });
-        }
-
-        if ($brand_id) {
-            $products = $products->where('brand_id', $brand_id);
-        }
-
-        switch ($order) {
-            case 1:
-                $products = $products->orderBy('created_at', 'desc')->paginate();
-                break;
-            case 2:
-                $products = $products->orderBy('price', 'asc')->paginate();
-                break;
-            case 3:
-                $products = $products->orderBy('price', 'desc')->paginate();
-                break;
-            case 4:
-                $products = $products->orderBy('name', 'asc')->paginate();
-                break;
-            case 5:
-                $products = $products->orderBy('name', 'desc')->paginate();
-                break;
-            default:
-                $products = $products->paginate();
-                break;
-        }
-
-        $products = $products->paginate();
-
-        $context = compact('category', 'products', 'categories', 'brands', 'params', 'banners');
-
-        return view('pages.category', $context);
-
     }
+
+    if ($category_id) {
+        $productsQuery->whereHas('categories', function ($query) use ($category_id) {
+            $query->where('category_id', $category_id);
+        });
+    }
+
+    if ($brand_id) {
+        $productsQuery->where('brand_id', $brand_id);
+    }
+
+    $productBrandIds = (clone $productsQuery)->pluck('brand_id')->unique()->toArray();
+
+    $brands = $brands->filter(fn($brand) => in_array($brand->id, $productBrandIds));
+
+    switch ($order) {
+        case 1:
+            $products = $productsQuery->orderBy('created_at', 'desc')->paginate();
+            break;
+        case 2:
+            $products = $productsQuery->orderBy('price', 'asc')->paginate();
+            break;
+        case 3:
+            $products = $productsQuery->orderBy('price', 'desc')->paginate();
+            break;
+        case 4:
+            $products = $productsQuery->orderBy('name', 'asc')->paginate();
+            break;
+        case 5:
+            $products = $productsQuery->orderBy('name', 'desc')->paginate();
+            break;
+        default:
+            $products = $productsQuery->paginate();
+            break;
+    }
+
+    $categoriesArray = [];
+    foreach ($products as $product) {
+        $ids = $product->categories()->pluck('id')->toArray();
+        $categoriesArray = array_merge($categoriesArray, $ids);
+    }
+
+    $categories = $categories->filter(fn($cat) => in_array($cat->id, $categoriesArray));
+
+    $context = compact('category', 'products', 'categories', 'brands', 'params', 'banners');
+    return view('pages.category', $context);
+}
+
 
 
 
