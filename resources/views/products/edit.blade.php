8d0a41eb92a88fae62dd5d598ee0e581791a34f6
David Correa <david.correav@gmail.com>
2025-07-14 01:18:09 -0500

    Discounts

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -56,7 +56,16 @@
                     {{Aire::select($variations, 'variation_id', "Variación")->groupClass('col-span-3')}}
                 @endif
 
-                {{Aire::input('discount', 'Descuento %')->id('discount')->min(0)->max(100)->groupClass('col-span-3')}}
+                {{Aire::input('discount', 'Descuento %')->id('discount')->min(0)->max(100)->groupClass('col-span-1')}}
+
+                <div class="col-span-2 flex items-center">
+                    {{ Aire::hidden('first_purchase_only')->value(0)}}
+                    <label class="relative inline-flex items-center cursor-pointer">
+                        <input @checked($product->first_purchase_only) type="checkbox" name='first_purchase_only' value="1" class="sr-only peer">
+                        <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+                        <span class="ml-3 text-sm font-medium text-gray-900">Aplicar solo en primera compra</span>
+                    </label>
+                </div>
 
                 {{Aire::input('package_quantity', 'Cantidad por Empaque')->id('package_quantity')->groupClass('col-span-3')}}
 
