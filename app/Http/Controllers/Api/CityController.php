0e6ff5127b873556619fefcbc3edc6ec612274dc
David Correa <david.correav@gmail.com>
2025-09-15 15:14:16 -0500

    Update cities list for registration form - restrict to 11 supported cities

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -13,7 +13,12 @@ class CityController extends Controller
      public  function index(Request $request){
         $state = $request->state;
      
-        $cities = City::whereStateId($state)->orderBy('name')->select(['id', 'name'])->get();
+        if ($state) {
+            $cities = City::whereStateId($state)->orderBy('name')->select(['id', 'name'])->get();
+        } else {
+            // Return all cities when no state is specified (for registration form)
+            $cities = City::orderBy('name')->select(['id', 'name'])->get();
+        }
 
         return $cities;
     }
