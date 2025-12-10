49b3b1df765ef2a9ed70039262526390d217549b
David Correa <david.correav@gmail.com>
2025-12-10 14:58:18 -0500

    test

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -135,14 +135,14 @@
                     data-add-to-cart 
                     data-product-id="{{ $product->id }}" 
                     @if($isVacationMode) disabled @endif
-                    class="bg-secondary p-2 mt-4 text-white hover:bg-gray2 flex px-4 text-xl font-semibold rounded-full items-center justify-start w-52 disabled:opacity-50 disabled:cursor-not-allowed">
+                    class="bg-secondary p-2 mt-4 text-white hover:bg-gray2 flex px-4 text-xl font-semibold rounded-full items-center justify-center w-52 disabled:opacity-50 disabled:cursor-not-allowed">
                 <span>¡Lo quiero! </span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                 </svg>
             </button>
             @if($isVacationMode)
-                <p class="text-xs text-gray-600 text-left mt-2 w-52 break-words">
+                <p class="text-xs text-gray-600 text-center mt-2 w-52 break-words">
                     Tuti está de vacaciones. Te esperamos nuevamente {{ $formattedVacationDate }}. ¡Gracias!
                 </p>
             @endif
